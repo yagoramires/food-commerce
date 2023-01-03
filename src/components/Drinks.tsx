96cd198e4dc44493fc8243fc1c7a01fbb0977424
yagox <yago.ramiresx@gmail.com>
2023-01-03 11:22:21 -0300

    axios config, json server config, fetch de dados com axios

--- src/components/Drinks.tsx
+++ src/components/Drinks.tsx
@@ -1,55 +1,25 @@
+import { getDrinks } from '../services/api'
 import Card from './Card'
 import FoodTitle from './FoodTitle'
 import Head from './Head'
+import { useEffect, useState } from 'react'
 
 const Drinks = () => {
-  const data = [
-    {
-      id: 1,
-      type: 'drink',
-      name: 'Coca-Cola',
-      price: 7.99,
-      image:
-        'https://carrefourbr.vtexassets.com/arquivos/ids/18904046/coca-cola-310ml-1.jpg?v=637590218201970000',
-    },
-    {
-      id: 2,
-      type: 'drink',
-      name: 'Guaraná Antártica',
-      price: 7.99,
-      image:
-        'https://giassi.vtexassets.com/arquivos/ids/507250/Refrigerante-Guarana-Antarctica-Lata-310ml.png?v=637994816574830000',
-    },
-    {
-      id: 3,
-      type: 'drink',
-      name: 'Matte Leão',
-      price: 6.99,
-      image:
-        'https://andinacocacola.vtexassets.com/arquivos/ids/155490/7891098040671_01.jpg?v=637722480149630000',
-    },
-    {
-      id: 4,
-      type: 'drink',
-      name: 'Fanta Uva',
-      price: 8.5,
-      image:
-        'http://d2r9epyceweg5n.cloudfront.net/stores/001/043/122/products/fanta-uva-3501-012134716f394f6f1a15698487625514-640-0.png',
-    },
-    {
-      id: 5,
-      type: 'drink',
-      name: 'Água',
-      price: 4.5,
-      image:
-        'https://www.imigrantesbebidas.com.br/bebida/images/products/full/2893-agua-mineral-crystal-sem-gas-500ml.jpg',
-    },
-  ]
+  const [drinks, setDrinks] = useState([])
+
+  useEffect(() => {
+    ;(async () => {
+      const drinkRequest = await getDrinks()
+
+      setDrinks(drinkRequest.data)
+    })()
+  }, [])
+
   return (
     <div className='flex flex-col gap-8 pt-4 sm:pb-16'>
-      <Head title='Hambúrgueres' description='Escolha seu hambúrguer e se surpreenda.' />
-      <FoodTitle name='Hambúrgueres' />
-      <Card data={data} />
+      <Head title='Bebidas' description='Escolha uma bebida para acompanhar o seu lanche.' />
+      <FoodTitle name='Bebidas' />
+      <Card data={drinks} />
     </div>
   )
 }
--- src/components/Pizzas.tsx
+++ src/components/Pizzas.tsx
@@ -1,62 +1,25 @@
+import { getPizzas } from '../services/api'
 import Card from './Card'
 import FoodTitle from './FoodTitle'
 import Head from './Head'
+import { useEffect, useState } from 'react'
 
 const Pizzas = () => {
-  const data = [
-    {
-      id: 1,
-      type: 'pizza',
-      name: 'Cattuperoni',
-      description:
-        'Massa Tradicional, Molho de Tomate, Queijo mussarela, Pepperoni, Catupiry® e Orégano.',
-      price: 64.9,
-      image: 'https://forneriaoriginal.com/wp-content/uploads/2020/04/27_catuperoni2.jpg',
-    },
-    {
-      id: 2,
-      type: 'pizza',
-      name: 'Portuguesa',
-      description:
-        'Massa Tradicional, Molho de Tomate, Queijo mussarela, Ovo, Tomate, Cebola, Azeitona, Presunto.',
-      price: 43.5,
-      image:
-        'https://www.receitasnestle.com.br/sites/default/files/srh_recipes/103eca6a504379a0df6f55155b8d607d.jpg',
-    },
-    {
-      id: 3,
-      type: 'pizza',
-      name: '4 Queijos',
-      description:
-        'Massa Tradicional, Molho de Tomate, Mussarela, Gorgonzola, Parmesão, Catupiry®.',
-      price: 55,
-      image:
-        'https://www.receitasnestle.com.br/sites/default/files/srh_recipes/d036cd01122da62bf581784f52d99b3a.jpg',
-    },
-    {
-      id: 4,
-      type: 'pizza',
-      name: 'Da casa',
-      description:
-        'Molho de Tomate, Queijo Mussarela, Calabreza, Azeitona Preta, Milho, Palmito, Catupiry® e Orégano.',
-      price: 17.5,
-      image: 'https://forneriaoriginal.com/wp-content/uploads/2016/10/a-moda-1.png',
-    },
-    {
-      id: 5,
-      type: 'pizza',
-      name: 'Camarão com Alho Poró',
-      description:
-        'Molho de Tomate, Mussarela, Cream Cheese Philadelphia, Camarões, Alho Poró e Orégano.',
-      price: 16,
-      image: 'https://forneriaoriginal.com/wp-content/uploads/2021/01/camarao-alho-poro1.png',
-    },
-  ]
+  const [pizzas, setPizzas] = useState([])
+
+  useEffect(() => {
+    ;(async () => {
+      const pizzaRequest = await getPizzas()
+
+      setPizzas(pizzaRequest.data)
+    })()
+  }, [])
+
   return (
     <div className='flex flex-col gap-8 pt-4 sm:pb-16'>
-      <Head title='Hambúrgueres' description='Escolha seu hambúrguer e se surpreenda.' />
-      <FoodTitle name='Hambúrgueres' />
-      <Card data={data} />
+      <Head title='Pizzas' description='Escolha seu hambúrguer e se surpreenda.' />
+      <FoodTitle name='Pizzas' />
+      <Card data={pizzas} />
     </div>
   )
 }
